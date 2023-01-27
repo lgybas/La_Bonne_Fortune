08e241e0b7758973f88cf047490bd03f05109183
Liane Gybas <liane.gybas@gmail.com>
2023-01-27 13:37:44 +0100

    initial files

---
+++ La_Bonne_Fortune.py
@@ -0,0 +1,32 @@
+#%% #some preparation:
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+from matplotlib import pyplot as plt
+from matplotlib import image as mpimg
+ 
+#%%
+
+
+
+#plt.title("Tree Image")
+#plt.xlabel("X pixel scaling")
+#plt.ylabel("Y pixels scaling")
+
+
+image = mpimg.imread("Resources/single_pink_tree_with_trunk.jpeg")
+plt.imshow(image)
+plt.axis('off')
+plt.show()
+
+#plt.savefig("test.png", bbox_inches='tight')
+
+#%%
+
+# Add to my streamlit app
+
+st.title("La Bonne Fortune :sweat_drops: :sunny: :seedling:", anchor=None)
+st.image(image)
+
+# %%
